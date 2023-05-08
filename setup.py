9edf354cbf8cda0fc09d556f836630fd2d645dd5
depowered <dpwr16@gmail.com>
2023-05-08 13:11:07 -0500

    initial commit - cookiecutter generated project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A computer vision model for detecting culverts in high-resolution digital elevation models.',
+    author='depowered',
+    license='MIT',
+)
