eece6c3f1fb67496edbb0e2618fa5106d418f64c
depowered <dpwr16@gmail.com>
2023-05-08 15:41:49 -0500

    apply formatting to cookiecutter files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A computer vision model for detecting culverts in high-resolution digital elevation models.',
-    author='depowered',
-    license='MIT',
+    version="0.1.0",
+    description="A computer vision model for detecting culverts in high-resolution digital elevation models.",
+    author="depowered",
+    license="MIT",
 )
