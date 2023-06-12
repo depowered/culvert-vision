89972183ebb52926db6945cd8139db870a321592
depowered <dpwr16@gmail.com>
2023-06-12 10:14:02 -0500

    add extract_vector_sources process

---
+++ src/data/vector/extract_vector_sources.py
@@ -0,0 +1,51 @@
+"""Module for downloading external vector sources."""
+from datetime import datetime
+from email.utils import format_datetime
+from pathlib import Path
+
+import requests
+from loguru import logger
+from pydantic import HttpUrl
+
+from src.settings import Settings
+
+
+def get_last_modified_dt(filepath: Path) -> None:
+    return datetime.fromtimestamp(filepath.lstat().st_mtime)
+
+
+def remote_is_newer(url: HttpUrl, filepath: Path) -> bool:
+    """Uses If-Modified-Since request header to determine if the remote file is
+    newer than the local copy.
+    [MDN If-Modified-Since](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/If-Modified-Since)"""
+    last_modified = get_last_modified_dt(filepath=filepath)
+    headers = {"If-Modified-Since": format_datetime(dt=last_modified)}
+    r = requests.head(url, headers=headers)
+    return r.status_code == 200
+
+
+def download_vector_source(url: HttpUrl, filepath: Path) -> None:
+    r = requests.get(url, stream=True)
+    with open(filepath, "wb") as f:
+        f.write(r.content)
+
+
+def extract_vector_sources(settings: Settings) -> None:
+    # Filter vector sources for those with a download_url
+    vector_sources = [v for _, v in settings.vector_sources.items() if v.download_url]
+
+    for vector_src in vector_sources:
+        filepath = vector_src.filepath
+        url = vector_src.download_url
+        if filepath.exists() and not remote_is_newer(url, filepath):
+            logger.info(f"EXTRACT: Local {filepath.name} is up-to-date")
+            continue
+        logger.info(f"EXTRACT: Downloading {filepath.name}")
+        # Ensure file directory exists
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        download_vector_source(url, filepath)
+
+
+if __name__ == "__main__":
+    settings = Settings()
+    extract_vector_sources(settings)
--- src/settings.py
+++ src/settings.py
@@ -9,11 +9,17 @@ DATA_DIR = PROJECT_DIR / "data/"
 LOG_DIR = PROJECT_DIR / "logs/"
 
 
+class LoadLayer(BaseModel):
+    layer: str
+    to_table: str
+
+
 class VectorSource(BaseModel):
     download_url: Optional[HttpUrl]
     filepath: Path
-    schemaname: str
-    tablename: str
+    schemaname: Optional[str]
+    tablename: Optional[str]
+    load_layers: list[LoadLayer]
 
 
 class Settings(BaseSettings):
@@ -41,12 +47,48 @@ class Settings(BaseSettings):
             filepath=DATA_DIR / "external/usgs/WESM.gpkg",
             schemaname="raw",
             tablename="usgs_wesm",
+            load_layers=[LoadLayer(layer="WESM", to_table="raw_usgs_wesm")],
         ),
         "usgs_opr_tesm": VectorSource(
             download_url="https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/OPR/FullExtentSpatialMetadata/OPR_TESM.gpkg",
             filepath=DATA_DIR / "external/usgs/OPR_TESM.gpkg",
             schemaname="raw",
             tablename="usgs_opr_tesm",
+            load_layers=[LoadLayer(layer="OPR_TILE_SMD", to_table="raw_usgs_opr_tesm")],
+        ),
+        "mndnr_culvert_inventory": VectorSource(
+            download_url="https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/struc_culvert_inventory_pub/gpkg_struc_culvert_inventory_pub.zip",
+            filepath=DATA_DIR / "external/mndnr/gpkg_struc_culvert_inventory_pub.zip",
+            load_layers=[
+                LoadLayer(layer="Bridge_Assessments", to_table="raw_mndnr_bridges"),
+                LoadLayer(
+                    layer="Stream_Crossing_Summary",
+                    to_table="raw_mndnr_stream_crossings",
+                ),
+                LoadLayer(
+                    layer="Culvert_Opening", to_table="raw_mndnr_culvert_openings"
+                ),
+            ],
+        ),
+        "mndnr_hydrography": VectorSource(
+            download_url="https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/water_dnr_hydrography/gpkg_water_dnr_hydrography.zip",
+            filepath=DATA_DIR / "external/mndnr/gpkg_water_dnr_hydrography.zip",
+            load_layers=[
+                LoadLayer(layer="dnr_hydro_features_all", to_table="raw_mndnr_lakes"),
+                LoadLayer(
+                    layer="dnr_rivers_and_streams", to_table="raw_mndnr_watercourses"
+                ),
+            ],
+        ),
+        "mndnr_watershed_suite": VectorSource(
+            download_url="https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/geos_dnr_watersheds/gpkg_geos_dnr_watersheds.zip",
+            filepath=DATA_DIR / "external/mndnr/gpkg_geos_dnr_watersheds.zip",
+            load_layers=[
+                LoadLayer(
+                    layer="dnr_watersheds_dnr_level_08_all_catchments",
+                    to_table="raw_mndnr_catchments_lvl08",
+                ),
+            ],
         ),
     }
 
