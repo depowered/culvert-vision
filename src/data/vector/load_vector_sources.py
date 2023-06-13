59d0e4618bc0aaf565c65c5661e3141d4491f946
depowered <dpwr16@gmail.com>
2023-06-13 12:29:34 -0500

    delete existing duckdb before loading

--- src/data/vector/load_vector_sources.py
+++ src/data/vector/load_vector_sources.py
@@ -1,3 +1,5 @@
+"""Module for loading raw data from external vector sources into and intermediate duckdb database."""
+import os
 from pathlib import Path
 
 import duckdb
@@ -7,24 +9,28 @@ from src.settings import Settings, VectorSource
 
 
 def load_layer(db: Path, from_gpkg: Path, layer: str, to_table: str) -> None:
-    # Ensure db directory exists
-    db.parent.mkdir(parents=True, exist_ok=True)
     con = duckdb.connect(str(db))
     con.install_extension("spatial")
     con.load_extension("spatial")
     query = f"""
-        CREATE OR REPLACE TABLE {to_table} AS
-        SELECT * FROM ST_Read($gpkg, sequential_layer_scan=TRUE, layer=$layer)
+        CREATE TABLE {to_table} AS
+        SELECT * FROM ST_Read('{from_gpkg}', sequential_layer_scan=TRUE, layer='{layer}');
     """
-    params = {
-        "gpkg": str(from_gpkg),
-        "layer": layer,
-    }
-    con.execute(query=query, parameters=params).commit()
+    con.execute(query).commit()
 
 
 def load_vector_sources(settings: Settings) -> None:
+    """Load vector sources into the duckdb database specified in settings.raw_data_pond.
+    WARNING: Process is DESTRUCTIVE. The existing database will be fully deleted before loading vector data.
+    """
     sources: list[VectorSource] = list(settings.vector_sources.values())
+    db = settings.raw_data_pond
+
+    # Always load into a fresh database
+    if db.exists():
+        os.remove(db)
+    # Ensure parent directory exists
+    db.parent.mkdir(parents=True, exist_ok=True)
 
     for src in sources:
         for lyr in src.load_layers:
@@ -32,7 +38,7 @@ def load_vector_sources(settings: Settings) -> None:
                 f"LOAD: Loading {lyr.layer} from {src.filepath.name} into table {lyr.to_table}"
             )
             load_layer(
-                db=settings.raw_data_pond,
+                db=db,
                 from_gpkg=src.filepath,
                 layer=lyr.layer,
                 to_table=lyr.to_table,
